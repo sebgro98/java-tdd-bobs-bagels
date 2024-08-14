a732e129f7458eabbf28910b021d45a918637efa
Sebastian <sebgro@kth.se>
2024-08-14 14:07:49 +0200

    Second test is now passing

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 public class Basket {
     ArrayList<String> bagelsInStore = new ArrayList<String>();
 
+    public Basket() {
+        bagelsInStore.add("Cheese Bagel");
+    }
+
     public boolean addBagel(String name) {
         if(name.isEmpty() || !bagelsInStore.contains(name)) {
             return false;
