d7daef22e417f5ff0baeeb3afa855e3e91407f9d
Sebastian <sebgro@kth.se>
2024-08-14 13:58:23 +0200

    First test is now passing

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -2,4 +2,11 @@ package com.booleanuk.core;
 
 public class Basket {
 
+    public boolean addBagel(String name) {
+        if(name.isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
 }
